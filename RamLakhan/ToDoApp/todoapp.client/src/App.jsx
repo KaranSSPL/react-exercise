91c408ede5947deeccb22a706a6f061e91072601
Karan <96464179+KaranSSPL@users.noreply.github.com>
2025-05-23 14:02:28 +0530

    Fix file structure.

---
+++ RamLakhan/ToDoApp/todoapp.client/src/App.jsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/App.css';
+import Dashboard from './Components/Dashboard/Dashboard'
+import Starred from './Components/StarTask/Starred';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MyContextProvider } from './global/MyContext';
+import Layout from './global/Layout';
+
+function App() {
+
+    return (
+
+        <BrowserRouter>
+            <MyContextProvider>
+                <Routes>
+                    <Route path="/" element={ <Layout> <Dashboard /> </Layout>} />
+
+                    <Route path="/starred" element={ <Layout> <Starred /> </Layout>} />
+                </Routes>
+            </MyContextProvider>
+        </BrowserRouter>
+    );
+
+}
+
+export default App;
